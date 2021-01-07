181ed96c14d8d2903c286a1f2dabb8214c039f70
durnfordrelay <63732564+durnfordrelay@users.noreply.github.com>
2021-01-07 17:20:05 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById('amtRaised').innerHTML = '£' + '8276'
+
+/*
+
+D = data[0]
+
+
+document.getElementById('file').value = D.distance_run;
+document.getElementById('dcText').innerHTML = 'We have achieved ' + '<a class="bolder">' + D.distance_run + 'km' + '</a>' + ' of our 1400km goal'
+document.getElementById('lastUpdated').innerHTML = 'Last Updated: ' + D.last_updated
+document.getElementById('amountRaised').innerHTML = D.amount_raised
+document.getElementById('amtRaised').innerHTML = '£8276'
+
+*/
